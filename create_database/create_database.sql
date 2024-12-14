7a15de7d5d5d508dbf24d05963eae3018660efc2
brenda0998 <brenda.sanchezh98@gmail.com>
2024-12-13 21:21:49 -0500

    tablas restantes

--- create_database/create_database.sql
+++ create_database/create_database.sql
@@ -45,6 +45,58 @@ CREATE TABLE sala (
     capacidad INT NOT NULL,
     tipo_sala VARCHAR(50) NOT NULL,
     id_cine INT NOT NULL,
-    CONSTRAINT FK_sala_cine FOREIGN KEY (id_cine) REFERENCES cine(id)
+    CONSTRAINT FK_cine_sala FOREIGN KEY (id_cine) REFERENCES cine(id)
+);
+GO
+
+--HORARIO
+CREATE TABLE horario (
+    id INT PRIMARY KEY IDENTITY(1,1),
+    fecha DATE NOT NULL,
+    hora_inicio TIME NOT NULL,
+    id_sala INT NOT NULL,
+    id_pelicula INT NOT NULL,
+    CONSTRAINT FK_sala_horario FOREIGN KEY (id_sala) REFERENCES sala(id),
+    CONSTRAINT FK_pelicula_horario FOREIGN KEY (id_pelicula) REFERENCES pelicula(id)
+);
+GO
+
+--METODO DE PAGO
+CREATE TABLE metodo_pago (
+    id INT PRIMARY KEY IDENTITY(1,1),
+    tipo_pago VARCHAR(255) NOT NULL,
+    descripcion VARCHAR(255),
+    banco_emisor VARCHAR(255),
+);
+GO
+
+--TRANSACCION
+CREATE TABLE transaccion (
+    id INT PRIMARY KEY IDENTITY(1,1),
+    fecha DATETIME NOT NULL,
+    monto FLOAT NOT NULL,
+    referencia_pago VARCHAR(100) NOT NULL,
+	estado VARCHAR(100) NOT NULL,
+	id_metodo_pago INT NOT NULL,
+    id_clientes INT NOT NULL,
+    CONSTRAINT FK_metodo_pago_transaccion FOREIGN KEY (id_metodo_pago) REFERENCES metodo_pago(id),
+    CONSTRAINT FK_clientes_transaccion FOREIGN KEY (id_clientes) REFERENCES clientes(id)
+);
+GO
+
+--ENTRADA
+CREATE TABLE entrada (
+    id INT PRIMARY KEY IDENTITY(1,1),
+    precio FLOAT NOT NULL,
+    estado VARCHAR(150) NOT NULL,
+    numero_asiento VARCHAR(10) NOT NULL,
+    tipo_entrada VARCHAR(150) NOT NULL,
+    fecha_compra DATE NOT NULL,
+    id_horario INT NOT NULL,
+    id_transaccion INT,
+    id_clientes INT,
+    CONSTRAINT FK_horario_entrada FOREIGN KEY (id_horario) REFERENCES horario(id),
+    CONSTRAINT FK_transaccion_entrada FOREIGN KEY (id_transaccion) REFERENCES transaccion(id),
+    CONSTRAINT FK_clientes_entrada FOREIGN KEY (id_clientes) REFERENCES clientes(id)
 );
 GO
